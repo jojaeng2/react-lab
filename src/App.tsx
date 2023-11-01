f1243b54cc1682f75510e843b480c155d5f64016
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2023-11-01 20:18:59 +0900

    react state 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CountryList from "./CountryList"
 
 export type CountryType = {
@@ -8,16 +8,29 @@ export type CountryType = {
 }
 
 const App = () => {
-  let list: Array<CountryType> = [
+  const [msg, setMsg] = useState<string>("World");
+  const [list, setList] = useState<Array<CountryType>>([
     {no:1, country:"1", visited:false},
     {no:2, country:"2", visited:true},
     {no:3, country:"3", visited:false},
     {no:4, country:"4", visited:false},
-  ];
+  ]);
+
+  const addResult = (x: number, y: number) => {
+    return (
+      <div className="card card-body bg-light mb-3">
+        {x} + {y} = {x + y}
+      </div>
+    );
+  };
 
   return (
     <div className='container'>
-      <CountryList countries={list}/>
+      <h2>Hello {msg}!</h2>
+      <div className='dash-style'>
+        {addResult(4, 3)}
+        <CountryList countries={list}/>
+      </div>
     </div>
   )
 }
--- src/CountryList.tsx
+++ src/CountryList.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { CountryType } from "./App";
+import CountryItem from "./CountryItem";
 
 type CountryListPropsType = {
     countries: Array<CountryType>;
@@ -8,12 +9,7 @@ type CountryListPropsType = {
 const CountryList = (props: CountryListPropsType) => {
     const list = props.countries;
     let countries = list.map((item, index) => {
-        return (
-            <li key={item.no}
-                className={item.visited ? "list-group-item active" : "list-group-item"}>
-                {item.country}
-            </li>
-        )
+        return <CountryItem key={item.no} countryitem={item}/>;
     })
     return <ul className="list-group">{countries}</ul>
 }
