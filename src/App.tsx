31d05e64bf3d83f8c855ca3fb395ab34ecf90dc1
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2023-11-02 22:00:45 +0900

    propsType 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,16 @@
-import React, { useState } from 'react'
-import CountryList from "./CountryList"
-
-export type CountryType = {
-  no: number;
-  country: string;
-  visited: boolean;
-}
+import React, { useState } from "react";
+import Calc from "./Calc";
 
 const App = () => {
-  const [msg, setMsg] = useState<string>("World");
-  const [list, setList] = useState<Array<CountryType>>([
-    {no:1, country:"1", visited:false},
-    {no:2, country:"2", visited:true},
-    {no:3, country:"3", visited:false},
-    {no:4, country:"4", visited:false},
-  ]);
-
-  const addResult = (x: number, y: number) => {
-    return (
-      <div className="card card-body bg-light mb-3">
-        {x} + {y} = {x + y}
-      </div>
-    );
-  };
+  const [x, setX] = useState<number>(100);
+  // const [y, setY] = useState<number>(200);
+  // const [oper, setOper] = useState<string>("&");
 
   return (
-    <div className='container'>
-      <h2>Hello {msg}!</h2>
-      <div className='dash-style'>
-        {addResult(4, 3)}
-        <CountryList countries={list}/>
-      </div>
+    <div>
+      <Calc x={x}/>
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
