1541e340812c34922fa349a75bb6001b22c0da8c
조정현[커머스ID개발] <jeong_hyun.jo@navercorp.com>
2024-06-08 19:00:52 +0900

    sample

---
+++ hello-react/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Component } from 'react';
+import ValidationSample from './ValidationSample';
+
+class App extends Component {
+  render() {
+    return ( 
+      <ValidationSample/>
+    )
+  }
+}
+
+
+export default App;
